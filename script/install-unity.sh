85244bd410d14c5e86f776b245ecafefb097888a
oneRain <onerain88@gmail.com>
2021-04-22 17:57:01 +0800

    chore: try cache unity

--- script/install-unity.sh
+++ script/install-unity.sh
@@ -2,7 +2,7 @@
 
 # See https://unity3d.com/get-unity/download/archive
 # to get download URLs
-UNITY_DOWNLOAD_CACHE="$(pwd)/unity_download_cache"
+UNITY_DOWNLOAD_CACHE="./unity_download_cache"
 UNITY_OSX_PACKAGE_URL="https://download.unity3d.com/download_unity/46dda1414e51/MacEditorInstaller/Unity-2017.2.0f3.pkg"
 UNITY_WINDOWS_TARGET_PACKAGE_URL="https://beta.unity3d.com/download/46dda1414e51/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2017.2.0f3.pkg"
 

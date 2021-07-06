d21fc0586557da4c1ee24af9fba763f98ace8f7a
oneRain <onerain88@gmail.com>
2021-07-06 11:25:55 +0800

    chore

--- script/package.sh
+++ script/package.sh
@@ -25,7 +25,7 @@ pack() {
     mkdir $dir
     echo $path
     echo $dir
-    rsync -avz $path $dir
+    cp -r $path $dir
     if [[ $platform == standard ]] ; then
         cp $standardReleasePath/$storage.$standard.dll $dir
         cp $standardReleasePath/$storage.$standard.pdb $dir
